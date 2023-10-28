e6e40025e5b3dc94b203fe0e69ad681a9faefc38
jmathew-rez <jmathew@rezilienthealth.com>
2023-10-27 22:37:11 -0500

    Adding an example project.

---
+++ example/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import useConfig from './hooks/useConfig'
+import Lobby from './components/Lobby'
+import Room from './components/Room'
+
+const App = () => {
+	const [joined, setJoined] = useState(false)
+	const {
+		isLoading: configLoading,
+		token,
+		identity,
+		roomName,
+		error: configError,
+		getToken
+	} = useConfig()
+
+	// Loading
+	if (configLoading) return 'get config...'
+
+	// error
+	if (configError) return `Error: ${configError.message}`
+
+	// Lobby
+	if (!joined) {
+		return (
+			<div style={{ textAlign: 'center' }}>
+				<Lobby
+					handleSubmit={data => {
+						getToken({ identity: data.username, roomName: data.roomName })
+						setJoined(true)
+					}}
+				/>
+			</div>
+		)
+	}
+
+	return (
+		<div className='App'>
+			<Room
+				token={token}
+				identity={identity}
+				roomName={roomName}
+				onDisconnected={() => setJoined(false)}
+			/>
+		</div>
+	)
+}
+export default App
